0b71e073b81f8e7bbee81725e91590c1d2335ceb
Milica <psymp12@nottingham.ac.uk>
2024-10-26 17:02:01 +0100

    renamed ButtonPressed() to ChangeScenePressed()

--- scripts/ChangeSceneButton.cs
+++ scripts/ChangeSceneButton.cs
@@ -5,23 +5,8 @@ public partial class ChangeSceneButton : Button
 {
 	[Export]
 	public string ScenePath = "add me!";
-	
-	private void ButtonPressed(){
-		//GD.Print("hello world");
-		//GD.Print("Next scene is: " + ScenePath);
-		
-		GetTree().ChangeSceneToFile(ScenePath);
-	}
-	
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("hi");
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	private void ChangeScenePressed() {
+		GetTree().ChangeSceneToFile(ScenePath);
 	}
 }
