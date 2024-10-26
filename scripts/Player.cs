8f4b366e6c717c0349930c722395e5c8edeb4077
Milica <psymp12@nottingham.ac.uk>
2024-10-26 21:17:58 +0100

    added sound file imports

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -7,6 +7,16 @@ public partial class Player : CharacterBody2D {
 	[Export] public int maxHP = 100;
 	[Export] public int currentHP = 100;
 
+	private DateTime lastAction = DateTime.MinValue;
+	private DateTime lastFootstep = DateTime.MinValue;
+
+	private float timeBetweenActions = 0.5f;
+	private float timeBetweenFootsteps = 0.5f;
+
+	private bool hasTimePassed(DateTime started, double length) {
+		return DateTime.Now > started.AddMilliseconds(length);
+	}
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 	}
@@ -17,8 +27,15 @@ public partial class Player : CharacterBody2D {
 		Velocity = input * speed;
 		MoveAndSlide();
 
-		if (Input.IsActionPressed("special")) {
+		if (Input.IsActionPressed("special") && hasTimePassed(lastAction, timeBetweenActions)) {
 			GD.Print("special");
+			lastAction = DateTime.Now;
+		}
+
+		if (hasTimePassed(lastFootstep, timeBetweenFootsteps)) {
+			var sfx = GetNode<AudioStreamPlayer>("SFX");
+			sfx.Play();
+			lastFootstep = DateTime.Now;
 		}
 	}
 }
